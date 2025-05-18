e3f6e523a4ba1baa4fca9a89c4209bcdf66a3869
HTKNgan007 <nganha2004nh@gmail.com>
2025-05-18 10:23:27 +0700

    tao du an

---
+++ Football_tournament/src/main/java/vn/ngaha/footballTournament/FootballTournamentApplication.java
@@ -0,0 +1,13 @@
+package vn.ngaha.footballTournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTournamentApplication.class, args);
+	}
+
+}
---
+++ Football_tournament/src/test/java/vn/ngaha/footballTournament/FootballTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ngaha.footballTournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
